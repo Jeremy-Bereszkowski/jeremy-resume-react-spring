ee9fd3567841fd7b5f4fa1e81604fa813839dcfc
s3539822 <41284457+s3539822@users.noreply.github.com>
2021-02-11 17:59:35 +1100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
+import Parallax from 'react-spring/renderprops-addons'
+import ParallaxLayer from 'react-spring/renderprops-addons'
 
 
 export default function Home() {
